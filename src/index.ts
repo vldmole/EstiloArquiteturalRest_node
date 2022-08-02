c75685dd0a2a421aa40ee32af3caf99ab3716215
Vilson Luiz Dalle Mole <vldmole@gmai.com>
2022-08-02 16:28:15 -0300

    Adicionado TypeScript e ts-node-dev e app basico com express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello World 01082022");
+import express, {Request, Response, NextFunction} from 'express';
+
+const app = express();
+
+app.get('/status', (req: Request, resp: Response, next: NextFunction) =>
+{
+   resp.status(200).send({ foo: 'bar tools'  });
+});
+
+app.listen(3000, 'localhost', () => console.log("listen on port 3000"));
